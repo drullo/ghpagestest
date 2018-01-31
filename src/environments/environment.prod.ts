c452c0288378b64b674ac1ffd7cb7deae883e9da
Dan Rullo <drullo@cleavelandprice.com>
2018-01-31 15:34:59 -0500

    did some stuff

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAwpPIgUSoJHpuh-pWf0dur9SbGHwnE1Co',
+    authDomain: 'deploytest-5855b.firebaseapp.com',
+    databaseURL: 'https://deploytest-5855b.firebaseio.com',
+    projectId: 'deploytest-5855b',
+    storageBucket: 'deploytest-5855b.appspot.com',
+    messagingSenderId: '836799760869'
+  }
 };
